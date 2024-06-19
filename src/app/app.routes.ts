8afccf92b194204fbfc71987bd37271f49ab1d2d
JoaquinMayor <joaquin88mayor@gmail.com>
2024-06-19 14:58:12 -0300

    navbar y sidvar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 export const routes: Routes = [
     {
         path:"",
-        component: LoginComponent,  
+        component: SidenavComponent,  
     }
 ];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/UsuarioService.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+  
+  nombreApellido :string = "";
 
+  constructor(private usuarioService:UsuarioService){}
+  ngOnInit(): void {
+    this.nombreApellido = this.usuarioService.usuario.getNombre + " " + this.usuarioService.usuario.getApellido;
+  }
+  
 }
